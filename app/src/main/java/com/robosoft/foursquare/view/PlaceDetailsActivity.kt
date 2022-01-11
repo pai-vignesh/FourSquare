1cef192d7b37f2c2dc8919293971c40c65a818b9
Vignesh Pai <55633408+VigneshPai-hub@users.noreply.github.com>
2022-01-11 18:25:04 +0530

    details activity completed

--- app/src/main/java/com/robosoft/foursquare/view/PlaceDetailsActivity.kt
+++ app/src/main/java/com/robosoft/foursquare/view/PlaceDetailsActivity.kt
@@ -1,12 +1,27 @@
 package com.robosoft.foursquare.view
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.core.view.WindowCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.robosoft.foursquare.R
 import com.robosoft.foursquare.databinding.ActivityPlaceDetailsBinding
 
 class PlaceDetailsActivity : AppCompatActivity() {
     private lateinit var binding: ActivityPlaceDetailsBinding
+    private lateinit var mapFragment: SupportMapFragment
+    private lateinit var googleMap: GoogleMap
+    private lateinit var currentLocation: Location
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityPlaceDetailsBinding.inflate(layoutInflater)
@@ -15,5 +30,43 @@ class PlaceDetailsActivity : AppCompatActivity() {
         WindowCompat.setDecorFitsSystemWindows(window, false)
 
 
+    }
+
+    private fun fetchLocation() {
+        if (ActivityCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_FINE_LOCATION
+            ) !=
+            PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_COARSE_LOCATION
+            ) !=
+            PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 101
+            )
+            return
+        }
+        val task = fusedLocationProviderClient.lastLocation
+        task.addOnSuccessListener { location->
+            currentLocation = location
+            Toast.makeText(this, currentLocation.latitude.toString() + "" +
+                    currentLocation.longitude, Toast.LENGTH_SHORT).show()
+
+            mapFragment = supportFragmentManager.findFragmentById(R.id.mapFragment) as SupportMapFragment
+            mapFragment.getMapAsync {
+                googleMap = it
+                val myLocation = LatLng(
+                    location.latitude,
+                    location.longitude
+                )
+                googleMap.animateCamera(
+                    CameraUpdateFactory.newLatLngZoom(
+                        myLocation,
+                        15.5f
+                    )
+                )
+            }
+        }
     }
 }
