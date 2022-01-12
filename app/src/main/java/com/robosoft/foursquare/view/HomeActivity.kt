0b3b02378e166930c8dc077549268e3f96d8ccde
vignesh pai <vigneshpai43@gmail.com>
2022-01-12 13:30:28 +0530

    added drawer

--- app/src/main/java/com/robosoft/foursquare/view/HomeActivity.kt
+++ app/src/main/java/com/robosoft/foursquare/view/HomeActivity.kt
@@ -1,9 +1,11 @@
 package com.robosoft.foursquare.view
 
+import android.content.Intent
 import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.view.WindowCompat
 import androidx.viewpager2.widget.ViewPager2
@@ -13,11 +15,14 @@ import com.google.android.material.tabs.TabLayout
 import com.robosoft.foursquare.R
 import com.robosoft.foursquare.adapter.MyFragmentPagerAdapter
 import com.robosoft.foursquare.databinding.ActivityHomeBinding
+import com.yarolegovich.slidingrootnav.SlidingRootNav
+import com.yarolegovich.slidingrootnav.SlidingRootNavBuilder
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
     private lateinit var myFragmentPagerAdapter: MyFragmentPagerAdapter
     private lateinit var viewPager: ViewPager2
+    private lateinit var slidingRootNav: SlidingRootNav
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
@@ -26,8 +31,18 @@ class HomeActivity : AppCompatActivity() {
         WindowCompat.setDecorFitsSystemWindows(window, false)
         viewPager = binding.pager
         viewPager.isUserInputEnabled = false;
+        slidingRootNav = SlidingRootNavBuilder(this).withToolbarMenuToggle(binding.topAppBar)
+            .withMenuOpened(false)
+            .withContentClickableWhenMenuOpened(false)
+            .withSavedState(savedInstanceState)
+            .withMenuLayout(R.layout.menu_left_drawer)
+            .inject();
+        
+
+
         setPagerAdapter()
     }
+
 
     private fun setPagerAdapter() {
         myFragmentPagerAdapter = MyFragmentPagerAdapter(supportFragmentManager, lifecycle)
