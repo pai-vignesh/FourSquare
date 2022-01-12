c42365b5b31ffb44452679937122ee34d39a20c0
Vignesh Pai <55633408+VigneshPai-hub@users.noreply.github.com>
2022-01-13 01:42:03 +0530

    added dialog

--- app/src/main/java/com/robosoft/foursquare/view/AddReview.kt
+++ app/src/main/java/com/robosoft/foursquare/view/AddReview.kt
@@ -4,16 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.view.WindowCompat
 import com.robosoft.foursquare.databinding.ActivityAddReviewBinding
-import androidx.core.app.ActivityCompat.startActivityForResult
-
-import android.content.Intent
 import android.net.Uri
-import android.provider.MediaStore
+import android.view.View
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.core.content.FileProvider
-import com.robosoft.foursquare.BuildConfig
-import java.io.File
-
+import com.robosoft.foursquare.util.CustomDialogClass
 
 class AddReview : AppCompatActivity() {
     private lateinit var binding: ActivityAddReviewBinding
@@ -28,6 +22,11 @@ class AddReview : AppCompatActivity() {
             selectImageFromGallery()
         }
 
+        val cdd = CustomDialogClass(this)
+
+        binding.submitBtn.setOnClickListener {
+            cdd.show()
+        }
     }
 
     private val selectImageFromGalleryResult = registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
